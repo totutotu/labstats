b0bce13bfbe5e23eb3c869994773b01cded76b35
totutotu <tuomo.torppa@gmail.com>
2018-12-09 17:11:30 +0200

    production connection

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
+let connection
 
-module.exports = mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  authSource: 'admin',
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASSWORD,
-  dbName : 'dev'
-})
+if (process.env.NODE_ENV === 'development') {
+  connection = mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    authSource: 'admin',
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASSWORD,
+    dbName : 'dev'
+  })
+} else {
+  connection = mongoose.connect(process.env.DB_URI)
+}
+
+module.exports = connection
