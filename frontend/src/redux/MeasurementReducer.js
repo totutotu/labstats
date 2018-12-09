21f111b8f0bb2fe1b7b4058182638aea86eec672
totutotu <tuomo.torppa@gmail.com>
2018-12-09 16:41:37 +0200

    Fixed travis and api endpoint

--- frontend/src/redux/MeasurementReducer.js
+++ frontend/src/redux/MeasurementReducer.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const ROOT_URL = 'http://localhost:8080/api'
+const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : 'http://labstats.herokuapp.com/api'
 
 const getMeasurementsAttempt = () => ({
   type: 'GET_MEASUREMENTS_ATTEMPT'
