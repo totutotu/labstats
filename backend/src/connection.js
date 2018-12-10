e340133edd0073aadfe4d5b6bf26427e900d8658
totutotu <tuomo.torppa@gmail.com>
2018-12-10 20:03:39 +0200

    Test env vars separated

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -10,11 +10,11 @@ if (process.env.NODE_ENV === 'development') {
     dbName: 'dev'
   })
 } else if (process.env.NODE_ENV === 'test') {
-  connection = mongoose.connect(process.env.DB_URI, {
+  connection = mongoose.connect(process.env.TEST_DB_URI, {
     useNewUrlParser: true,
     authSource: 'admin',
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASSWORD,
+    user: process.env.TEST_DB_USER,
+    pass: process.env.TEST_DB_PASSWORD,
     dbName: 'test'
   })
 } else {
