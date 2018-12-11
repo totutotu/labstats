89436d75d58d33b6ae55ff7dbed85b562253b412
totutotu <tuomo.torppa@gmail.com>
2018-12-11 08:36:01 +0200

    force https

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 require('./connection.js')
 const path = require('path')
 
-const enforce = require('express-sslify')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -13,15 +12,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const port = process.env.PORT || 3000
 const routes = require('./routes')
 
-app.use('/api', routes)
-
 if (process.env.NODE_ENV !== 'development' || process.env.NODE_ENV !== 'test') {
-  app.use(enforce.HTTPS())
   app.use(express.static(path.join(__dirname, 'dist')))
+  app.use((request, response) => {
+    if (!request.secure) {
+      response.redirect('https://' + request.headers.host + request.url)
+    }
+  })
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'))
   })
 }
+
+app.use('/api', routes)
 
 if (process.env.NODE_ENV !== 'test') app.listen(port, () => console.log('App listening on port ' + port))
 
